042d6e8b7e70ed84a9b55d1711faad4290590d17
Marcin Krystianc <marcin.krystianc@gmail.com>
2020-11-06 08:16:51 +0100

    dev-0, dev-1

--- benchmark.sh
+++ benchmark.sh
@@ -1,40 +1,68 @@
 #!/bin/bash
 SLN="SanitisedNet471"
-REPEATS=2
+REPEATS=50
 
 mytime=$(date -u)
 mydir=$(realpath "`dirname \"$0\"`")
 
+results0=$mydir/"results0.txt"
 results1=$mydir/"results1.txt"
-results2=$mydir/"results2.txt"
 global=$mydir/"global.json"
+global0=$mydir/"global0.json"
 global1=$mydir/"global1.json"
-global2=$mydir/"global2.json"
 
 # exit when any command fails
 set -e
 
 # Read the dotnet version
+cp $global0 $global
+version0=$(cd $mydir/$SLN && dotnet --version)
+
 cp $global1 $global
 version1=$(cd $mydir/$SLN && dotnet --version)
 
-cp $global2 $global
-version2=$(cd $mydir/$SLN && dotnet --version)
-
 # Insert test run markers
+echo "Starting the benchmark. mydir=$mydir, SLN=$SLN, Version=$version0, Repeats=$REPEATS, Time=$mytime" >> $results0
 echo "Starting the benchmark. mydir=$mydir, SLN=$SLN, Version=$version1, Repeats=$REPEATS, Time=$mytime" >> $results1
-echo "Starting the benchmark. mydir=$mydir, SLN=$SLN, Version=$version2, Repeats=$REPEATS, Time=$mytime" >> $results2
 
-dt=
+dt=0
+dt0=0
+dt1=0
 i=0
 while [[ $i -le $REPEATS ]]
 do
 	if [ $i -gt 0 ]
 	then
-		eta=$(echo "($dt1 + $dt2) * ($REPEATS - $i + 1)"| bc)
-		echo "ETA:$eta"
+		eta=$(echo "($dt0 + $dt1) * ($REPEATS - $i + 1)"| bc)
+		echo "dt0:$dt0 dt1:$dt1 ETA:$eta"
 	else
 		echo "warmup run"
+	fi
+	
+	##############
+	# Prepare 0
+	##############
+	cp $global0 $global
+	(cd $mydir/$SLN && git clean -fxd) >> /dev/null
+
+	##############
+	# Measure 0
+	##############
+	starttime0=$(date +%s.%N)
+
+	##############
+	# Command 0
+	##############
+	(cd $mydir/$SLN && dotnet restore -clp:summary) >> /dev/null
+
+	endtime0=$(date +%s.%N)
+	dt0=$(echo "$endtime0 - $starttime0" | bc)
+
+	if [ $i -gt 0 ]
+	then
+		echo "$dt0" >> $results0
+	else
+		echo "warmup run:$dt0" >> $results0
 	fi
 	
 	##############
@@ -55,38 +83,12 @@ do
 
 	endtime1=$(date +%s.%N)
 	dt1=$(echo "$endtime1 - $starttime1" | bc)
-
+	
 	if [ $i -gt 0 ]
 	then
 		echo "$dt1" >> $results1
 	else
 		echo "warmup run:$dt1" >> $results1
-	fi
-	
-	##############
-	# Prepare 2
-	##############
-	cp $global2 $global
-	(cd $mydir/$SLN && git clean -fxd) >> /dev/null
-
-	##############
-	# Measure 2
-	##############
-	starttime2=$(date +%s.%N)
-
-	##############
-	# Command 1
-	##############
-	(cd $mydir/$SLN && dotnet restore -clp:summary) >> /dev/null
-
-	endtime2=$(date +%s.%N)
-	dt2=$(echo "$endtime2 - $starttime2" | bc)
-	
-	if [ $i -gt 0 ]
-	then
-		echo "$dt2" >> $results2
-	else
-		echo "warmup run:$dt2" >> $results2
 	fi
 
 	
