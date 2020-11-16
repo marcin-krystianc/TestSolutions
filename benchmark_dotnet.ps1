88b33e867e67aa93d20864da5f4c360f878acdd5
Marcin Krystianc <marcin.krystianc@gmail.com>
2020-11-16 11:16:02 +0100

    benchmark_dotnet

---
+++ benchmark_dotnet.ps1
@@ -0,0 +1,12 @@
+$solutions = @("SanitisedNet471", "NuGet.Client", "OrchardCore", "orleans")
+For ($i=0; $i -lt $solutions.Length; $i++) 
+{
+	$solution = $solutions[$i]
+	For ($j=0; $j -lt 2; $j++) 
+	{
+		Copy-Item "global0.json" -Destination "global.json"
+		.\NuGet.Client\scripts\perftests\RunPerformanceTests.ps1 -nugetClientFilePath "..\dotnet\dotnet.exe" -resultsFilePath results0_$solution.csv -solutionFilePath $solution -skipCleanRestores -skipColdRestores -skipNoOpRestores -staticGraphRestore -iterationCount 100
+		Copy-Item "global1.json" -Destination "global.json"
+		.\NuGet.Client\scripts\perftests\RunPerformanceTests.ps1 -nugetClientFilePath "..\dotnet\dotnet.exe" -resultsFilePath results1_$solution.csv -solutionFilePath $solution -skipCleanRestores -skipColdRestores -skipNoOpRestores -staticGraphRestore -iterationCount 100
+	}
+}
