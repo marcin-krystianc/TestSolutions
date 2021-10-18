d3ec8cc0058556986ba0a98aac17aa4626f0aff8
Marcin Krystianc <marcin.krystianc@gmail.com>
2021-10-18 12:18:22 +0200

    test build ability

---
+++ SimpleAppWithLibs3CentralisedOptimized/Lib2/Foo.cs
@@ -0,0 +1,12 @@
+﻿
+using Newtonsoft.Json.Bson.Converters;
+namespace Lib2
+{
+    public class Lib
+    {
+        public void Method1()
+        {
+            var tmp = new BsonDataObjectIdConverter();
+        }
+    }
+}
