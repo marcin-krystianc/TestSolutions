de5ae70ad877b52dbea42a088f9abb926e62a381
Marcin Krystianc <marcin.krystianc@gmail.com>
2022-06-15 16:22:19 +0200

    FSharpDefineConstants

---
+++ FSharpDefineConstants/CSharpProject/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpProject;
+
+public static class Say
+{
+#if FOO
+    public static void Hello(string name) => Console.WriteLine($"Foo {name}");
+#else
+    public static void Hello(string name) => Console.WriteLine($"Hello {name}");
+#endif
+}
---
+++ FSharpDefineConstants/FSharpProject/Library.fs
@@ -0,0 +1,8 @@
+﻿namespace FSharpProject
+
+module Say =
+#if FOO
+    let hello name = printfn "Foo %s" name
+#else
+    let hello name = printfn "Hello %s" name
+#endif
