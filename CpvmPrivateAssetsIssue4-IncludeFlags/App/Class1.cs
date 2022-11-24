00e7f4e59095770ad67759d34d1781460da811c0
Marcin Krystianc <marcin.krystianc@gmail.com>
2022-11-24 14:54:08 +0100

    CpvmPrivateAssetsIssue4-IncludeFlags

---
+++ CpvmPrivateAssetsIssue4-IncludeFlags/App/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace App
+{
+    public class CLass1
+    {
+        void Method1()
+        {
+            var type = typeof(JsonConverter);
+            if (type != null) throw new Exception();
+        }
+    }
+}
